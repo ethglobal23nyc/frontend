bb32270f1481747f608fb1ab9f0e85d376847606
Ulysses Kee <ulysseskcw96@gmail.com>
2023-09-24 03:19:07 -0400

    add ens resolver

--- src/components/recent-contributions.tsx
+++ src/components/recent-contributions.tsx
@@ -1,10 +1,26 @@
+"use client";
+
+import { useEffect } from "react";
+import { useEnsAddress } from "wagmi";
+
 export default function RecentContributions() {
+  const name = "vitalik.eth";
+  const ensName = useEnsAddress({ name, chainId: 1 });
+
   return (
     <div className="space-y-8">
       <div className="flex items-center">
         <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Stable Diffusion</p>
-          <p className="text-sm text-muted-foreground">stablediffusion.eth</p>
+          <p className="text-sm font-medium leading-none">vitalik.eth</p>
+          <p className="text-sm text-muted-foreground">
+            {ensName.isLoading ? (
+              <p>...Loading</p>
+            ) : ensName.isSuccess ? (
+              ensName.data
+            ) : (
+              "Error"
+            )}
+          </p>
         </div>
         <div className="ml-auto font-medium">+$1,999.00</div>
       </div>
