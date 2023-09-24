b667d8be1c9895876515e745b3e970d95a6b1879
Ulysses Kee <ulysseskcw96@gmail.com>
2023-09-23 22:36:23 -0400

    add data marketplace

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,49 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+interface SponsorItem {
+  name: string;
+  categories: string[];
+  bounty: number;
+}
+
+const fakeData: SponsorItem[] = [
+  { name: "hello kitty", categories: ["cat", "kitty"], bounty: 100 },
+  { name: "dogs", categories: ["dog", "puppy"], bounty: 200 },
+];
+
 export default function Page() {
-  return <h1>dashboard dashboard</h1>;
+  return (
+    <div className="flex flex-col min-h-screen py-2">
+      <h1 className="font-bold text-6xl pl-10 pt-8">Data Marketplace</h1>
+      <main className="flex flex-col flex-1 py-20 px-20 text-center">
+        <Table className="w-3/6">
+          <TableCaption>Open Sponsor Models</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Name</TableHead>
+              <TableHead>Categories</TableHead>
+              <TableHead className="text-right">Bounty</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {fakeData.map((item, idx) => (
+              <TableRow key={idx}>
+                <TableCell className="font-medium">{item.name}</TableCell>
+                <TableCell>{item.categories.join(", ")}</TableCell>
+                <TableCell className="text-right">{item.bounty}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </main>
+    </div>
+  );
 }
--- src/components/provider-wrapped-app.tsx
+++ src/components/provider-wrapped-app.tsx
@@ -4,7 +4,7 @@ import {
   w3mConnectors,
   w3mProvider,
 } from "@web3modal/ethereum";
-import { Web3Modal } from "@web3modal/react";
+import { Web3Button, Web3Modal } from "@web3modal/react";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { arbitrum, mainnet, polygon, filecoin } from "wagmi/chains";
 
@@ -27,7 +27,9 @@ export default function ProviderWrappedApp({
   return (
     <>
       <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>
-
+      <div className="absolute top-10 right-10">
+        <Web3Button />
+      </div>
       <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
     </>
   );
