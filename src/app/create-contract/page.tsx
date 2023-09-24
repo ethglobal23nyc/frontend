ff83b40e885743f2b5e0b0d6edaa7973a7943f04
Bill Bai <31229051+billbai0102@users.noreply.github.com>
2023-09-23 23:59:01 -0400

    form

--- src/app/create-contract/page.tsx
+++ src/app/create-contract/page.tsx
@@ -4,6 +4,9 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 
+import { cn } from "@/lib/utils"
+import { format } from "date-fns"
+
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -15,18 +18,30 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Popover, PopoverContent, PopoverTrigger } from "@radix-ui/react-popover";
+import { CalendarIcon } from "@radix-ui/react-icons";
+import { Calendar } from "@/components/ui/calendar";
+import React from "react";
 
 const formSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
+  datatype: z.string(),
+  expiration: z.date(),
+  model: z.string()
 });
 
 export default function Page() {
+
+  const [date, setDate] = React.useState<Date>()
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
+      datatype: "",
+      expiration: new Date,
+      model: ""
     },
   });
 
@@ -38,7 +53,7 @@ export default function Page() {
   }
 
   return (
-    <Form {...form}>
+    <Form {...form} className="small">
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
@@ -47,7 +62,7 @@ export default function Page() {
             <FormItem>
               <FormLabel>Username</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input placeholder="pepehands" {...field} />
               </FormControl>
               <FormDescription>
                 This is your public display name.
@@ -56,6 +71,81 @@ export default function Page() {
             </FormItem>
           )}
         />
+
+        <FormField
+          control={form.control}
+          name="datatype"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Requested Data Type</FormLabel>
+              <FormControl>
+                <Input placeholder="Health Data" {...field} />
+              </FormControl>
+              <FormDescription>
+                The kind of data you'd like constituents to provide.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+      <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[280px] justify-start text-left font-normal",
+            !date && "text-muted-foreground"
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+
+    <FormField
+          control={form.control}
+          name="model"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>ML Model IPFS address</FormLabel>
+              <FormControl>
+                <Input placeholder="https://ipfs.io/ipfs/..." {...field} />
+              </FormControl>
+              <FormDescription>
+                Add a link to where your ML model is stored on IPFS.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* <FormField
+          control={form.control}
+          name="expiration"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Requested Data Type</FormLabel>
+              <FormControl>
+                <Input placeholder="Health Data" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        /> */}
+
         <Button type="submit">Submit</Button>
       </form>
     </Form>
