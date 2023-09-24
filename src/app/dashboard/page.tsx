7a1228ec51f24b97791147df0280e75627d28350
Ulysses Kee <ulysseskcw96@gmail.com>
2023-09-23 23:43:32 -0400

    add .nvmrc

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { CategorySearch } from "@/components/categories-search";
 import {
   Table,
   TableBody,
@@ -10,7 +11,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 
 interface SponsorItem {
@@ -24,9 +25,20 @@ const fakeData: SponsorItem[] = [
   { name: "dogs", categories: ["dog", "puppy"], bounty: 200 },
 ];
 
+const getCategories = (data: SponsorItem[]): string[] => {
+  const categories: string[] = [];
+
+  data.forEach((item) => {
+    categories.push(...item.categories);
+  });
+
+  return categories;
+};
+
 export default function Page() {
   const router = useRouter();
   const { address, isConnected } = useAccount();
+  const [selectedCategory, setSelectedCategory] = useState<string>("");
 
   // useEffect(() => {
   //   if (!isConnected) {
@@ -38,6 +50,7 @@ export default function Page() {
     <div className="flex flex-col min-h-screen py-2">
       <h1 className="font-bold text-6xl pl-20 pt-10">Data Marketplace</h1>
       <main className="flex flex-col flex-1 py-20 px-20 text-center">
+        <CategorySearch categories={getCategories(fakeData)} />
         <Table className="w-3/6">
           <TableCaption>Open Sponsor Models</TableCaption>
           <TableHeader>
