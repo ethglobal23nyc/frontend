9a2e85647b25ee02e4dabfcd92629f15a8f175e4
Ulysses Kee <ulysseskcw96@gmail.com>
2023-09-24 08:14:06 -0400

    add web3inbox stuff

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   env: {
     walletConnectProjectId: "500baad82f2812145feb7e023ea0c32c",
+    NOTIFY_API_SECRET: "4be21d1d-96bb-49e5-8bbf-e75896da7ba4",
   },
   webpack: (config, context) => {
     if (config.plugins) {
